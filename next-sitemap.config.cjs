3a62e82347f05af2d9fe34814231c29604b52218
PrinceJoshwa <sprincejoshwa85@gmail.com>
2025-07-24 17:03:44 +0530

    Add Site map

---
+++ next-sitemap.config.cjs
@@ -0,0 +1,6 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: 'https://www.hyvefreelance.com', // your real domain here
+  generateRobotsTxt: true,
+  sitemapSize: 5000,
+};
