d49075863eeb2b4aed2e1a238b88ea5e927317fc
PrinceJoshwa <sprincejoshwa85@gmail.com>
2025-07-24 18:42:29 +0530

    Add Site map for all pages

--- next-sitemap.config.cjs
+++ next-sitemap.config.cjs
@@ -3,4 +3,20 @@ module.exports = {
   siteUrl: 'https://www.hyvefreelance.com', // your real domain here
   generateRobotsTxt: true,
   sitemapSize: 5000,
+
+  async additionalPaths(config) {
+    const paths = [
+      '/blog',
+      '/contact',
+      '/about',
+      '/privacy',
+      '/terms',
+    ];
+
+    return paths.map((url) => ({
+      loc: url,
+      changefreq: 'monthly',
+      priority: 0.7,
+    }));
+  },
 };
